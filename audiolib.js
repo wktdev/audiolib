ca9934ef9042032962b07e70fa72d247e5881931
wktdev <wktdev@gmail.com>
2014-12-25 23:33:05 -0800

    Update audiolib.js

--- audiolib.js
+++ audiolib.js
@@ -26,7 +26,30 @@ function audioFileLoader(fileDirectory) {
 
 };
 
-// Example 
-var snare = audioFileLoader("snare.mp3");
 
-window.addEventListener("click",snare.play,false)
+function audioBatchLoader(obj) {
+
+    for (prop in obj) {
+        obj[prop] = audioFileLoader(obj[prop])
+
+    }
+    return obj
+
+}
+
+//________________________________________________________ Example 
+
+// var snare = audioFileLoader("snare.mp3");
+// window.onclick = function(){sound.snare.play(audioContext.currentTime)}
+
+
+
+//_________________________________________________________Example
+// var sound = audioBatchLoader({
+//     kick: "kick.mp3",
+//     snare: "snare.mp3",
+//     hihat: "hihat.mp3",
+//     shaker: "shaker.mp3"
+
+// })
+// window.onclick = function(){sound.snare.play(audioContext.currentTime)}
